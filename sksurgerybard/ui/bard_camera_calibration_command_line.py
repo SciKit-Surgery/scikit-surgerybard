0cbe3ff3798298fe8c0e5e9b61b6cddd9361c442
Mian Asbat ahmad <mianasbatahmad@eduroam-int-dhcp-97-228-159.ucl.ac.uk>
2019-03-25 13:04:23 +0000

    Issue #1: Arguments added

---
+++ sksurgerybard/ui/bard_camera_calibration_command_line.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+#
+# """Command line processing"""
+#
+#
+# def run_demo(input_file, output_file, width,
+#              height, size, verbose):
+#     """ Run the application """
+#     pass
