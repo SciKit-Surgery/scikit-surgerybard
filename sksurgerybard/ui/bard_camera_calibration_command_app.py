8b54ec660d1fd160e80fd7f2a66ad9d8faa21d78
Mian Asbat ahmad <mianasbatahmad@eduroam-int-dhcp-97-228-159.ucl.ac.uk>
2019-03-26 14:57:39 +0000

    Issue #1: cabera callibration implemented using opencv example code, link provided in code as comment.

--- sksurgerybard/ui/bard_camera_calibration_command_app.py
+++ sksurgerybard/ui/bard_camera_calibration_command_app.py
@@ -3,7 +3,8 @@
 """Command line processing"""
 
 import argparse
-# import numpy as np
+import numpy as np
+from glob import glob
 import cv2
 
 from sksurgerybard import __version__
@@ -64,13 +65,8 @@ def main(args=None):
 
     args = parser.parse_args(args)
 
-    # This section will read all the file names from the text file
-    # and store it in a list input_files.
-    with open(args.input, "r") as ins:
-        array = []
-        for line in ins:
-            array.append(line.strip())
-    input_files = array
+    # Gets the directory containing images.
+    input_dir = args.input
 
     # output_file = args.output
     #
@@ -79,16 +75,55 @@ def main(args=None):
     # height = args.ycorners
     #
     # size = args.size
-    for item in input_files:
-        file_name = item
-        view = cv2.imread(file_name)
 
-        height, width = view.shape[:2]
+    # Calibration code added from the following link
+    # https://opencv-python-tutroals.readthedocs.io/en/
+    # latest/py_tutorials/py_calib3d/py_calibration/py_calibration.html
 
-        if height == 0 and width == 0:
-            print("WARNING: Can not open ", file_name)
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-        print("Read: ", file_name)
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((14*10, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:14, 0:10].T.reshape(-1, 2)
+
+    # Arrays to store object points and image points from all the images.
+    obj_points = []  # 3d point in real world space
+    img_points = []  # 2d points in image plane.
+
+    images = glob.glob(input_dir)
+
+    if len(images) < 3:
+        raise RuntimeError('Less than 3 images processed successfully')
+
+    for fname in images:
+
+        # print(fname)
+        img = cv2.imread(fname)
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (14, 10), None)
+
+        # If found, add object points, image points (after refining them)
+        if ret is True:
+            obj_points.append(objp)
+
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            img_points.append(corners2)
+
+            # Line is commented because displaying
+            # images are not our requirement here.
+
+            # Draw and display the corners
+            # img = cv2.drawChessboardCorners(img, (14,10), corners2,ret)
+            # cv2.imshow('img',img)
+            # cv2.waitKey(5000)
+
+    # Now to do the calibration
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], None, None)
+
 
 
     # run_demo(input_file, output_file, width, height, size, args.verbose)
