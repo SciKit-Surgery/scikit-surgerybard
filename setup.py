03c11cd6eb3a634ba2d12e5cc959822395efd70f
Mian Asbat ahmad <mianasbatahmad@eduroam-int-dhcp-97-228-159.ucl.ac.uk>
2019-03-27 14:49:42 +0000

    Issue #1: lines length reduced to below 80 in setup.py

--- setup.py
+++ setup.py
@@ -14,10 +14,12 @@ setup(
     name='scikit-surgerybard',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description='scikit-surgerybard is a Python package based on SNAPPY for Basic Augmented Reality Demo (BARD)',
+    description='scikit-surgerybard is a Python package based on SNAPPY for '
+                'Basic Augmented Reality Demo (BARD)',
     long_description=long_description,
     long_description_content_type='text/x-rst',
-    url='https://weisslab.cs.ucl.ac.uk/WEISS/SoftwareRepositories/SNAPPY/scikit-surgerybard',
+    url='https://weisslab.cs.ucl.ac.uk/WEISS/SoftwareRepositories/SNAPPY/'
+        'scikit-surgerybard',
     author='Mian Asbat Ahmad',
     author_email='rmapaah@ucl.ac.uk',
     license='BSD-3 license',
@@ -58,8 +60,10 @@ setup(
     entry_points={
         'console_scripts': [
             'sksurgerybard=sksurgerybard.__main__:main',
-            'sksurgerybard-cl=sksurgerybard.ui.bard_camera_calibration_command_line:main',
-            'sksurgerybard-app=sksurgerybard.ui.bard_camera_calibration_command_app:main',
+            'sksurgerybard-cl=sksurgerybard.ui.bard_camera_calibration_'
+            'command_line:main',
+            'sksurgerybard-app=sksurgerybard.ui.bard_camera_calibration_'
+            'command_app:main',
         ],
     },
 )
