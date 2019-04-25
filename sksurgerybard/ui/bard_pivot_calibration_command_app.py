67d708cd2dc84be725fe5fb31675ee2f5e1273c7
Mian Asbat ahmad <mianasbatahmad@eduroam-int-dhcp-97-228-159.ucl.ac.uk>
2019-04-25 13:43:48 +0100

    Issue #2: pivot calibration file structure added

---
+++ sksurgerybard/ui/bard_pivot_calibration_command_app.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+"""Basic Augmented Reality Demo BARD Application"""
+
+import glob
+import numpy as np
+import cv2
+
+
+def run_demo(input_file, output_file):
+    """ Demo app, to perform camera calibration """
+    print(input_file)
+    print(output_file)
+    pass
+
