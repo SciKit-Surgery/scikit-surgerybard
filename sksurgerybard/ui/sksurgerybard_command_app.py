efca16e46a24d4452fdc4861775f47bc8a32230e
Mian Asbat ahmad <mianasbatahmad@eduroam-int-dhcp-97-228-159.ucl.ac.uk>
2019-05-17 11:11:17 +0100

    Issue #4:
    Models added.
    Unwanted camera calibration values removed.
    Model overlayed on video source done.

--- sksurgerybard/ui/sksurgerybard_command_app.py
+++ sksurgerybard/ui/sksurgerybard_command_app.py
@@ -5,125 +5,11 @@
 # pylint: disable=import-error
 
 import sys
-import time
-import cv2
+from PySide2.QtWidgets import QApplication
 import six
 import numpy as np
 import sksurgerycore.configuration.configuration_manager as config
-from PySide2 import QtCore, QtWidgets
-from PySide2.QtCore import Slot
-import sksurgeryutils.utils.image_utils as iu
-# import sksurgeryvtk.models.surface_model_loader as sml
-
-
-# pylint: disable=too-many-instance-attributes
-
-class DemoGui(QtWidgets.QWidget):
-    """ Demo GUI, with 2 QLabel side by side."""
-    def __init__(self, camera, width, height, grab, milliseconds,
-                 calibration_path):
-        super().__init__()
-
-        calibration_data = np.load(calibration_path)
-        six.print_(calibration_data['mtx'])
-        six.print_(calibration_data['dist'])
-        six.print_(calibration_data['rvecs'])
-        six.print_(calibration_data['tvecs'])
-
-        self.layout = QtWidgets.QHBoxLayout()
-
-        if not isinstance(camera, str) and camera < 0:
-            self.cap = None
-        else:
-            self.cap = cv2.VideoCapture(camera)  # pylint: disable=no-member
-            self.cap.set(3, width)
-            self.cap.set(4, height)
-
-            if not self.cap.isOpened():
-                raise ValueError("Unable to open camera:" + str(camera))
-
-            grabbed, self.frame = self.cap.read()
-            if not grabbed:
-                raise RuntimeError("Failed to grab first frame.")
-
-            if self.frame.shape[0] != height:
-                raise ValueError("Grabbed image has wrong number of rows:"
-                                 + str(self.frame.shape[0]))
-
-            if self.frame.shape[1] != width:
-                raise ValueError("Grabbed image has wrong number of columns:"
-                                 + str(self.frame.shape[1]))
-
-            self.image_label = QtWidgets.QLabel("Hello Image")
-            self.image_label.setAlignment(QtCore.Qt.AlignCenter)
-            self.layout.addWidget(self.image_label)
-
-        self.setLayout(self.layout)
-
-        self.grab = QtCore.QTimer()
-        self.grab.setInterval(grab)
-
-        self.clock = QtCore.QTimer()
-        self.clock.setInterval(milliseconds)
-        # pylint: disable=maybe-no-member
-        # self.clock.timeout.connect(self.update_clock)
-
-        self.elapsed = QtCore.QElapsedTimer()
-
-        self.number_frames = 0
-        self.total_time_to_grab = 0.0
-        self.total_time_to_decode = 0.0
-        self.total_time_to_display = 0.0
-
-        self.elapsed.start()
-        self.clock.start()
-
-        if isinstance(camera, str) or camera >= 0:
-            # pylint: disable=maybe-no-member
-            self.grab.timeout.connect(self.update_image)
-            self.grab.start()
-        else:
-            self.setMinimumSize(400, 400)
-
-    @Slot()
-    def update_image(self):
-        """Updates the image."""
-
-        # Force updating the GUI to get the latest possible time.
-        # self.text.update()
-
-        start_time = time.time()
-
-        # Then grab image
-        grabbed = self.cap.grab()
-        if not grabbed:
-            raise RuntimeError("Failed to grab frame number:"
-                               + str(self.number_frames))
-
-        # Independently work out the total time grabbing.
-        grab_time = time.time()
-        self.total_time_to_grab += (grab_time - start_time)
-
-        # Then decode image
-        self.cap.retrieve(self.frame)
-
-        # OpenCV does BGR, Qt wants RGB
-        # pylint: disable=no-member
-        rgb_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
-
-        # Independently work out the total time decoding.
-        decode_time = time.time()
-        self.total_time_to_decode += (decode_time - grab_time)
-
-        pixmap = iu.image_to_pixmap(rgb_frame)
-        self.image_label.setPixmap(pixmap)
-
-        # Independently work out the total time displaying.
-        display_time = time.time()
-        self.total_time_to_display += (display_time - decode_time)
-
-        self.number_frames += 1
-
+from sksurgeryutils.common_overlay_apps import OverlayOnVideoFeed
 
 
 def run_demo(config_file):
@@ -138,19 +24,21 @@ def run_demo(config_file):
 
     configuration_data = configuration_manager.get_copy()
 
-    camera = configuration_data['camera']['source']
-    width = configuration_data['camera']['width']
-    height = configuration_data['camera']['height']
-    grab = configuration_data['camera']['grab']
-    clock = configuration_data['camera']['clock']
-    fullscreen = configuration_data['camera']['fullscreen']
+    video_source = configuration_data['camera']['source']
     calibration_path = configuration_data['calibrationData']['path']
+    models_path = configuration_data['models']['models_dir']
 
-    app = QtWidgets.QApplication([])
+    calibration_data = np.load(calibration_path)
+    six.print_(calibration_data['mtx'])
+    six.print_(calibration_data['dist'])
 
-    widget = DemoGui(camera, width, height, grab, clock, calibration_path)
-    if fullscreen:
-        widget.showFullScreen()
-    widget.show()
+    app = QApplication([])
+
+    viewer = OverlayOnVideoFeed(video_source)
+
+    viewer.add_vtk_models_from_dir(models_path)
+
+    # start the viewer
+    viewer.start()
 
     return sys.exit(app.exec_())
