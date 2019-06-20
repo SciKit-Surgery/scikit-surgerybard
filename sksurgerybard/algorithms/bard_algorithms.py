91fdac286ec279d7970c5ce809170321a8049c5f
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2019-06-20 14:08:47 +0100

    Issue #12 setting default camera params [ci skip]

--- sksurgerybard/algorithms/bard_algorithms.py
+++ sksurgerybard/algorithms/bard_algorithms.py
@@ -5,6 +5,33 @@ import numpy as np
 from sksurgerycore.configuration.configuration_manager import \
         ConfigurationManager
 
+def configure_camera (camera_config):
+    """
+    Configures the camera
+    """
+    
+    video_source = None
+    calibration_path = None
+    mtx33d = np.array([1000.0, 0.0, 320.0, 0.0, 1000.0 , 240.0, 0.0, 0.0, 1.0])
+    mtx33d = np.reshape(mtx33d, (3,3))
+    dist5d = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
+    if camera_config:
+        video_source = camera_config.get('source')
+        calibration_path = camera_config.get('calibration')
+        
+        if calibration_path:
+            calibration_data = np.load(calibration_path)
+            mtx33d = calibration_data['mtx']
+            dist15d = calibration_data['dist']
+
+        if not video_source:
+            print ("WARNING, no video source in config, setting to 0")
+            video_source = 0
+    else:
+        print ("WARNING, no camera parameters in config file, trying some default values")
+    
+    return video_source, mtx33d, dist5d
+ 
 def configure_bard(configuration_file):
     """
     Parses the BARD configuration file, and prepares output for
@@ -19,31 +46,29 @@ def configure_bard(configuration_file):
 
     configuration_data = configurer.get_copy()
 
+    camera_config = configuration_data.get('camera')
+    video_source, mtx33d, dist15d = configure_camera(camera_config)
+
+    models_path = None
+    ref_points = None
+    reference2model_file = None
+    model_config = configuration_data.get('models')
+    if model_config:
+        models_path = model_config.get('models_dir')
+        ref_points = model_config.get('ref_file')
+        reference2model_file = model_config.get('reference_to_model')
+    
+
     using_pointer = False
-    video_source = configuration_data.get(
-        'camera').get('source')
-    calibration_path = configuration_data.get(
-        'camera').get('calibration')
-    models_path = configuration_data.get(
-        'models').get('models_dir')
-    ref_points = configuration_data.get(
-        'models').get('ref_file')
-    reference2model_file = configuration_data.get(
-        'models').get('reference_to_model')
+    ref_pointer_file = None
+    pointer_tip_file = None
+    pointer_config = configuration_data.get('pointerData')
     if 'pointerData' in configuration_data:
-        ref_pointer_file = configuration_data.get(
-            'pointerData').get('pointer_tag_file')
-        pointer_tip_file = configuration_data.get(
-            'pointerData').get('pointer_tag_to_tip')
+        ref_pointer_file = pointer_config.get('pointer_tag_file')
+        pointer_tip_file = pointer_config.get('pointer_tag_to_tip')
         using_pointer = True
 
-    calibration_data = np.load(calibration_path)
-
-    mtx33d = calibration_data['mtx']
-
-    dist15d = calibration_data['dist']
-
-
+  
     ref_data = np.loadtxt(ref_points)
     reference2model = np.loadtxt(reference2model_file)
 
--- sksurgerybard/ui/sksurgerybard_command_app.py
+++ sksurgerybard/ui/sksurgerybard_command_app.py
@@ -144,7 +144,7 @@ def run_demo(config_file):
                         reference2model, using_pointer, ref_point_data)
 
     viewer.add_vtk_models_from_dir(models_path)
-
+    
     matrix = create_vtk_matrix_from_numpy(reference2model)
     for actor in viewer.vtk_overlay_window.foreground_renderer.GetActors():
         actor.SetUserMatrix(matrix)
@@ -160,4 +160,5 @@ def run_demo(config_file):
         viewer.vtk_overlay_window.add_vtk_actor(pointer_tip_sphere.actor)
 
     viewer.start()
+
     sys.exit(app.exec_())
