d5fdb3a8ceba3d8888d0ef87f7c0220e45260e01
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2019-07-02 13:51:25 +0100

    Issue #21 modified setup and requirements to use pyside version

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         'six>=1.10',
         'numpy>=1.11',
         'glob2',
+        'PySide2<=5.11.0',
         'opencv-contrib-python',
         'scikit-surgerycore',
         'scikit-surgeryutils',
