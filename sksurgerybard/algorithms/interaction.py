50e0a8decf464f9a08077044b50c2194c6019670
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2020-03-12 15:22:54 +0000

    Issue #31 added logic to footswitch to disable hotkeys on linux

--- sksurgerybard/algorithms/interaction.py
+++ sksurgerybard/algorithms/interaction.py
@@ -1,5 +1,8 @@
 """Callback for dealing with interaction events in BARD"""
 
+from platform import system
+from subprocess import run 
+
 class BardKBEvent:
     """
     Handles keyboard events for bard
@@ -18,10 +21,42 @@ class BardKBEvent:
 class BardFootSwitchEvent:
     """
     Handles footswitch events for bard
+    This is for the footswitch in our lab, 
+    which plugs into USB and has three buttons, that
+    return ctrl-alt[5,6,7]
     """
+    def __init__(self):
+        #disable ctrl-alt-f[] events on linux systems
+        if system() == 'Linux':
+            try:
+                _ = run(['setxkbmap', '-option', 'srvrkeys:none'])
+            except:
+                print("Failed to disable ctrl-alt-f[]",
+                      "using the footpedal may have unpredictable results")
+
+
+    def __call__(self, event, _event_type_not_used):
+        print(event.GetKeySym())
+
+    def __del__(self):
+        #reenable ctrl-alt-f[] events on linux systems
+        print("killing footswitch")
+        if system() == 'Linux':
+            try:
+                #_ = run(['setxkbmap', '-option'])
+                print("resetting keyboard")
+                _ = run(['setxkbmap'])
+                _ = run(['setxkbmap', '-option'])
+            except:
+                print("Failed to reset xkbmap srvrkeys, sorry.")
 
 
 class BardMouseEvent:
     """
     handles mouse events
     """
+    def __init__(self):
+        return
+
+    def __call__(self, event, _event_type_not_used):
+        return
