914e0a2d153bf9aab582d77739abef7decb795e0
Stephen Thompson <s.thompson@ucl.ac.uk>
2020-03-31 15:34:47 +0100

    Issue #32 more testing

--- tests/test_speech_interation.py
+++ tests/test_speech_interation.py
@@ -3,13 +3,17 @@
 from time import sleep
 import sys
 import pytest
+TEST_OK = False
 try:
     import sksurgerybard.algorithms.speech_interaction as speech
 except ModuleNotFoundError:
     sys.path.insert(0, 'tests/test_utilities/')
     import sksurgerybard.algorithms.speech_interaction as speech
+    TEST_OK = True
 
 
 def test_BardSpeechInteractor():
     config = {}
     bard_speech = speech.BardSpeechInteractor(config, None)
+    if TEST_OK: 
+        print("Using fake VoiceRecognitionService to proceed with test")
