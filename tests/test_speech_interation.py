fa45a97ee0600e690de29a797b6505a5b6ed2ff9
Stephen Thompson <s.thompson@ucl.ac.uk>
2020-04-01 12:34:47 +0100

    Issue #32 test is working, needs to check for exceptions

--- tests/test_speech_interation.py
+++ tests/test_speech_interation.py
@@ -3,17 +3,71 @@
 from time import sleep
 import sys
 import pytest
-TEST_OK = False
+
 try:
-    import sksurgerybard.algorithms.speech_interaction as speech
+    #we do this because bard_config_algorithms doesn't throw module not found 
+    from sksurgerybard.algorithms.speech_interaction import BardSpeechInteractor
 except ModuleNotFoundError:
     sys.path.insert(0, 'tests/test_utilities/')
-    import sksurgerybard.algorithms.speech_interaction as speech
-    TEST_OK = True
+
+from sksurgerybard.algorithms.bard_config_algorithms import \
+                configure_speech_interaction
+
+
+class WritePointerEvent(Exception):#pylint: disable=missing-class-docstring
+    pass
+class CycleAnatomyEvent(Exception):#pylint: disable=missing-class-docstring
+    pass
+class NextTargetEvent(Exception):#pylint: disable=missing-class-docstring
+    pass
+class TurnOnAllEvent(Exception):#pylint: disable=missing-class-docstring
+    pass
+class VisibilityToggleEvent(Exception):#pylint: disable=missing-class-docstring
+    pass
+class ChangeOpacityEvent(Exception):#pylint: disable=missing-class-docstring
+    pass
+
+class _FakeVisualisationControl:
+    def cycle_visible_anatomy_vis(self): # pylint: disable=no-self-use
+        """Raises an error so we know when it's run"""
+        raise CycleAnatomyEvent
+
+    def next_target(self): # pylint: disable=no-self-use
+        """Raises an error so we know when it's run"""
+        raise NextTargetEvent
+
+    def turn_on_all_targets(self): # pylint: disable=no-self-use
+        """Raises an error so we know when it's run"""
+        raise TurnOnAllEvent
+
+    def visibility_toggle(self, _): # pylint: disable=no-self-use
+        """Raises an error so we know when it's run"""
+        raise VisibilityToggleEvent
+
+    def change_opacity(self, _): # pylint: disable=no-self-use
+        """Raises an error so we know when it's run"""
+        raise ChangeOpacityEvent
 
 
 def test_BardSpeechInteractor():
-    config = {}
-    bard_speech = speech.BardSpeechInteractor(config, None)
-    if TEST_OK: 
-        print("Using fake VoiceRecognitionService to proceed with test")
+    config = {
+        "timeout for command" : 1,
+        "sensitivities" : [1.0],
+        "interval": 10,
+        "recogniser" : "testing",
+        "test signals" : [
+            ["start_listen", False],
+            ["google_api_not_understand", False],
+            ["google_api_request_failure", "the internet is broken"],
+            ["voice_command", "next"],
+            ["voice_command", "map"],
+            ["voice_command", "clear"],
+            ["start_processing_request", False],
+            ["unknown_command", "what's this"],
+            ["voice_command", "quit"]],                
+            }
+    bard_speech = configure_speech_interaction(config, _FakeVisualisationControl())
+
+    sleep(2.0)
+
+    bard_speech.stop_listener()
--- tests/test_utilities/sksurgeryspeech/algorithms/voice_recognition_service.py
+++ tests/test_utilities/sksurgeryspeech/algorithms/voice_recognition_service.py
@@ -4,8 +4,11 @@ class VoiceRecognitionService(QObject):
     """A fake Voice recognition service, to enable us to do some
     testing on sksurgerybard.algorithms.speech_interaction without 
     loading the audio stack required for VoiceRecognitionService
+    It implements only the testing functionality of 
+    sksurgeryspeech.algorithms
     """
-    
+
+     
     start_listen = Signal()
     stop_timer = Signal()
     google_api_not_understand = Signal()
@@ -18,15 +21,52 @@ class VoiceRecognitionService(QObject):
         print("WARNING: Initialised a fake VoiceRecognitionService, " +
               "only to be used for testing")
         super(VoiceRecognitionService, self).__init__()
+        self.timeout_for_command = config.get("timeout for command", None)
+        self.recogniser = config.get("recogniser", "testing")
+        if self.recogniser != "testing":
+            raise ValueError("Using test VoiceRecognitionService with " +
+                             "invalid recognisor keyvalue, ", recognisor)
+        self._test_signals = config.get("test signals", None)
+        if self.recogniser == "testing" and self._test_signals is None:
+            raise KeyError("Asked for testing, with no test signals")
+
+        self.interval = config.get("interval", 10)
+        self.timer = None
     
     def run(self):
-        pass
+        self.timer = QTimer()
+        self.timer.setInterval(self.interval)
+        self.timer.timeout.connect(self._test_trigger)
+        self.stop_timer.connect(self.__stop)
+        self.timer.start()
 
     def request_stop(self):
-        pass
+        self.timer.stop()
+        QThread.msleep(self.interval * 3)
 
-    def listen_for_keyword(self):
-        pass
-
-    def listen_to_command(self):
-        pass
+    def _test_trigger(self):
+        """
+        Used for testing, looks for a list of signals to send, and
+        sends the next one.
+        """
+        if len(self._test_signals) > 0:
+            #I did try using eval to avoid the following stack of
+            #elifs but it didn't seem to work.
+            #eval('self.%s.emit(%s)', signal[0], signal[1])
+            signal = self._test_signals.pop(0)
+            if signal[0] == 'stop_timer':
+                self.stop_timer.emit()
+            elif signal[0] == 'start_listen':
+                self.start_listen.emit()
+            elif signal[0] == 'google_api_not_understand':
+                self.google_api_not_understand.emit()
+            elif signal[0] == 'google_api_request_failure':
+                self.google_api_request_failure(signal[1])
+            elif signal[0] == 'voice_command':
+                self.voice_command.emit(signal[1])
+            elif signal[0] == 'start_processing_request':
+                self.start_processing_request.emit()
+            else:
+                print('Unknown signal (%s, %s)', signal[0], signal[1])
+        else:
+            self.voice_command.emit('quit')
