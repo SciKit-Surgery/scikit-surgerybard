785eb55f089a57ceb6de5c89531bf826e73342c7
Stephen Thompson <s.thompson@ucl.ac.uk>
2020-05-18 11:18:41 +0100

    Issue #46 added test for quaternion to matrix

--- tests/algorithms/test_registration_2d3d.py
+++ tests/algorithms/test_registration_2d3d.py
@@ -19,3 +19,22 @@ def test_rvec_to_quaterion():
     assert quaternion[1] == 1.0 * math.sin(math.pi/4.0)
     assert quaternion[2] == 0.0
     assert quaternion[3] == 0.0
+
+
+def test_quaterion_to_matrix():
+    """
+    Test conversion on a 90 degree rotation about y axis.
+    """
+    quaternion = np.array([math.cos(math.pi/4.0), 0.0,
+                           1.0 * math.sin(math.pi/4.0), 0.0])
+
+    rot_mat = reg._quaternion_to_matrix(quaternion) # pylint: disable=protected-access
+
+    rot_mat1 = np.eye(3, dtype=np.float64)
+
+    rot_mat1[0, 0] = 0.0
+    rot_mat1[0, 2] = 1.0
+    rot_mat1[2, 0] = -1.0
+    rot_mat1[2, 2] = 0.0
+
+    assert np.allclose(rot_mat, rot_mat1, rtol=1e-05, atol=1e-10)
