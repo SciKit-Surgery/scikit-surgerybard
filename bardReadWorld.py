156cc0fd0b6c3ebaa9db65097fc5acbe9b186439
Matt Clarkson <m.clarkson@ucl.ac.uk>
2020-07-01 15:16:19 +0100

    Issue #52: Remove unneeded files.

--- bardReadWorld.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-#  -*- coding: utf-8 -*-
-import sys
-
-from sksurgerybard.ui.bard_read_load_world_points_command_line import main
-
-if __name__ == "__main__":
-    sys.exit(main(sys.argv[1:]))
