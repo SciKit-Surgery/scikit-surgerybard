3183d78895c172a2499028e708b2ffdb43dc56ba
Stephen Thompson <s.thompson@ucl.ac.uk>
2021-03-18 13:59:21 +0000

    Issue #61 lint the test

--- tests/widgets/test_bard_overlay.py
+++ tests/widgets/test_bard_overlay.py
@@ -2,7 +2,6 @@
 
 """ Tests for BARD configuration module. """
 
-import numpy as np
 import sksurgerybard.widgets.bard_overlay_app as boa
 
 
@@ -12,6 +11,5 @@ def test_valid_config():
     """
     file_name = 'config/reference_with_model.json'
     calib_dir = 'data/calibration/matts_mbp_640_x_480/'
-    
-    bard_overlay = boa.BARDOverlayApp(file_name, calib_dir)
 
+    _bard_overlay = boa.BARDOverlayApp(file_name, calib_dir)
