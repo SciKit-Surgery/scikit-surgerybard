6fb9c7dbf62218eac3e3a0fde4429544d53bbadd
Stephen Thompson <s.thompson@ucl.ac.uk>
2021-04-28 15:29:02 +0100

    Issue #61 put tracking in separate module

--- sksurgerybard/widgets/bard_overlay_app.py
+++ sksurgerybard/widgets/bard_overlay_app.py
@@ -20,51 +20,9 @@ from sksurgerybard.algorithms.bard_config_speech import \
     configure_speech_interaction
 from sksurgerybard.algorithms.visualisation import BardVisualisation
 from sksurgerybard.algorithms.pointer import BardPointerWriter
+from sksurgerybard.tracking.bard_tracking import setup_tracker
 
 # pylint: disable=too-many-instance-attributes, too-many-branches
-
-def setup_tracker(configuration):
-    """
-    BARD Internal function to configure an ArUco tracker
-    and return a tracker object. Could be modified to set
-    up another sksurgery tracker (e.g. nditracker)
-    """
-    tracker_config = {}
-    rigid_bodies = []
-    model_config = configuration.get('models', None)
-    if model_config is not None:
-        ref_filename = model_config.get('ref_file', None)
-        if ref_filename is None:
-            raise ValueError("Model configuration does not include ref_file")
-
-        rigid_bodies.append({
-                'name' : 'reference',
-                'filename' : ref_filename,
-                'aruco dictionary' : 'DICT_ARUCO_ORIGINAL'
-                })
-
-    pointer_config = configuration.get('pointerData', None)
-    if pointer_config is not None:
-        pointer_filename = pointer_config.get('pointer_tag_file', None)
-        if pointer_filename is None:
-            raise ValueError("Pointer config does not include pointer_tag_file")
-
-        rigid_bodies.append({
-                'name' : 'pointer',
-                'filename' : pointer_filename,
-                'aruco dictionary' : 'DICT_ARUCO_ORIGINAL'
-                })
-
-    tracker_config['rigid bodies'] = rigid_bodies
-
-    _video_source, mtx33d, dist5d, _dims = configure_camera(configuration)
-    tracker_config['video source'] = 'none'
-    tracker_config['camera projection'] = mtx33d
-    tracker_config['camera distortion'] = dist5d
-    tracker_config['aruco dictionary'] = 'DICT_4X4_50'
-    tracker_config['smoothing buffer'] = 3
-
-    return ArUcoTracker(tracker_config)
 
 class BARDOverlayApp(OverlayBaseApp):
     """
