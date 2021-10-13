bbf0e69766c95c581601827d28ce5db533d14278
Stephen Thompson <s.thompson@ucl.ac.uk>
2021-10-13 16:05:12 +0100

    Issue #76 added a temporary _version.py

---
+++ sksurgerybard/_version.py
@@ -0,0 +1,8 @@
+""" 
+    A temporary version file, which should be overwritten when you run
+    versioneer install
+"""
+
+def get_versions():
+    """Get version information or return default if unable to do so."""
+    return {"version": "0+unknown"}
